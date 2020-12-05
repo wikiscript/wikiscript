58e349dede953d5bdced5650cd5de226a1e7909e
Gerald Bauer <gerald.bauer@gmail.com>
2020-12-05 19:33:32 +0100

    up

--- countries/pages.rb
+++ countries/pages.rb
@@ -1,7 +1,12 @@
-require 'csvreader'
+############################################
+# to run use:
+#     ruby countries/pages.rb
+
+require 'webget'   ## note: includes/pulls in csvreader too
 
 
-rows = CsvHash.read( '../data/countries.csv' )
+
+rows = CsvHash.read( './data/countries.csv' )
 
 
 pp rows
@@ -29,10 +34,19 @@ base_url='https://en.wikipedia.org/w/index.php?title={title}&action=raw'
 
 rows.each do |row|
   title = row['Name']
-  url = base_url.gsub( '{title}', title )
-  puts "   >#{title}<   try url=>#{url}<"
+  url  = base_url.gsub( '{title}', title )
+  path = "#{title.gsub( ' ', '_' )}.txt"
+  puts "   >#{title}<   path=>#{path}<, try url=>#{url}<"
+
+  ## test create text file in tmp
+  ##   check if () and , can be used in filename too e.g.
+  ##    Archipelago of San Andrés, Providencia and Santa Catalina
+  ##     => Archipelago_of_San_Andrés,_Providencia_and_Santa_Catalina.txt
+  File.open( "./countries/tmp/#{path}", 'w:utf-8') {|f| f.write( 'test' ) }
 end
 
+puts "  #{rows.size} row(s)"
+puts
 
 
 puts "bye"
